fdfe90d76072e0d5b85f5054aba22c12e243ec4b
Johannes Waldmann <johannes.waldmann@htwk-leipzig.de>
2017-01-30 19:37:24 +0100

    update

--- examples/DominatingSet.hs
+++ examples/DominatingSet.hs
@@ -4,6 +4,8 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# language LambdaCase #-}
 {-# language NoMonomorphismRestriction #-}
+{-# language KindSignatures #-}
+{-# language ExplicitForAll #-}
 
 import Prelude hiding (and,or,not,(&&),(||))
 import qualified Prelude as P
@@ -25,6 +27,7 @@ main = getArgs >>= \ case
   [ w, s ] -> void $ run methods (read w) (read s)
   [ w] -> search methods (read w)
 
+methods :: [Method]
 methods = -- Binaries :
           SumBits :
           -- SumBit :
@@ -46,6 +49,7 @@ search how w = do
           Just xss -> go $ pred $ sum $ concat xss
   go $ w^2
 
+run :: [Method] -> Int -> Int -> IO (Maybe [[Int]])
 run methods w s = do
   as <- forM methods $ \ how -> A.async $ single how w s
   (_, r) <- A.waitAnyCancel as
@@ -68,6 +72,7 @@ single how w s = do
         print result
         return Nothing
 
+present :: forall a (t :: * -> *). (Foldable t, Show a, Num a, Eq a, Enum a) => String -> Int -> t [a] -> IO ()
 present msg s xss = do
     putStrLn msg
     let form = unwords . map (\ case 0 -> "."; 1-> "K") 
@@ -83,12 +88,15 @@ problem :: (MonadState s m, HasSAT s)
   -> m [[Bit]]
 problem how w s = do
   b <- allocate w 
-  break_symmetries [ transpose, reverse, map reverse ] b
-  assert_symmetries ( --  transpose . reverse :
+  when True $ break_symmetries 
+        [  transpose, reverse, map reverse 
+        -- transpose . reverse, reverse . map reverse
+        ] b
+  when True $ assert_symmetries (  transpose . reverse :
                      -- reverse . map reverse :
                      --  map reverse :
-		     transpose :
-		      reverse :
+		     -- transpose :
+		     -- reverse :
 		     []
 		    )  b
 
@@ -102,39 +110,58 @@ problem how w s = do
     assertClause $ get p : map get (neighbours p)
   return b
 
+distances :: [(Int,Int)]
+distances = do
+  (dx,dy) <- (,) <$> [-2..2] <*> [-2..2]
+  guard $ dx^2 + dy^2 == 5
+  return (dx,dy)
+
+neighbours :: (Int, Int) -> [(Int, Int)]
 neighbours (x,y) = do
-        (dx,dy) <- (,) <$> [-2..2] <*> [-2..2]
-        guard $ dx^2 + dy^2 == 5
-        return (x+dx,y+dy)
+  (dx,dy) <- distances
+  return (x+dx,y+dy)
   
+allocate :: forall (m :: * -> *) a s. (Variable a, HasSAT s, MonadState s m) => Int -> m [[a]]
 allocate w = replicateM w $ replicateM w exists
 
+assert_symmetries :: forall (t :: * -> *) (m :: * -> *) s (t1 :: * -> *) (t2 :: * -> *). (HasSAT s, MonadState s m, Foldable t2, Foldable t1, Foldable t) => t (t1 [Bit] -> t2 [Bit]) -> t1 [Bit] -> m ()
 assert_symmetries ops b = do
   forM_ ops $ \ op -> 
     assert $ Bits (concat b) === Bits (concat $ op b)
 
+break_symmetries :: forall (t :: * -> *) (m :: * -> *) s (t1 :: * -> *) a (t2 :: * -> *). (Orderable a, HasSAT s, MonadState s m, Foldable t2, Foldable t1, Foldable t) => t (t1 [a] -> t2 [a]) -> t1 [a] -> m ()
 break_symmetries ops b = do
   forM_ ops $ \ op -> 
     assert $ concat b <=? concat (op b)
 
 -- * try to reduce a solution
 
+dominated :: A.Array (Int, Int) Bool -> Bool
 dominated a = P.and $ do
   p <- A.indices a
   return $ P.or $ do
     q <- p : neighbours p
     return $ at a q
 
+at :: forall i. A.Ix i => A.Array i Bool -> i -> Bool
 at a q = if A.inRange (A.bounds a) q then a A.! q else False
 
+closed_neighbours :: (Int, Int) -> [(Int, Int)]
+closed_neighbours p = p : neighbours p
+
+reduce :: forall (t :: * -> *). Foldable t => t [Bool] -> [[Bool]]
 reduce xss = 
   let w = length xss
-      b = ((1,1),(w,w))
+      b = ((1::Int,1::Int),(w,w))
       a = A.listArray ((1,1),(w,w)) $ concat xss
       next a = do
         (p,True) <- A.assocs a
         let b = a A.// [(p,False)]
-        guard $ dominated b
+        -- guard $ dominated b
+        guard $ and $ (P.any (at b) $ neighbours p) : do 
+           q <- neighbours p
+           guard $ A.inRange (A.bounds b) q
+           return $ P.any (at b) $ closed_neighbours q
         return b
       go a = case next a of
          [] -> a
@@ -174,8 +201,10 @@ addBitsM cin x y = do
         return $ r:rs
   Bits <$> add2 false (unbits x) (unbits y)
 
+unbits :: Bits -> [Bit]
 unbits (Bits xs) = xs
 
+halfAddM :: forall (m :: * -> *) s. (HasSAT s, MonadState s m) => Bit -> Bit -> m (Bit, Bit)
 halfAddM x y = do
   r <- fuN (\ [x, y] -> x /= y ) [x,y]
   c <- exists
@@ -189,13 +218,16 @@ halfAddM x y = do
   return (r, c)
 
 
+fullAddM :: forall (m :: * -> *) s. (HasSAT s, MonadState s m) => Bit -> Bit -> Bit -> m (Bit, Bit)
 fullAddM = fullAddM_explicit
 
+fullAddM_half :: forall (m :: * -> *) s. (HasSAT s, MonadState s m) => Bit -> Bit -> Bit -> m (Bit, Bit)
 fullAddM_half x y z = do
   (r1,c1) <- halfAddM x y
   (r2,c2) <- halfAddM r1 z
   return (r2, c1 || c2)
   
+fullAddM_explicit :: forall (m :: * -> *) s. (HasSAT s, MonadState s m) => Bit -> Bit -> Bit -> m (Bit, Bit)
 fullAddM_explicit x y z = do
   r <- xorN [x, y, z] -- 8 clauses
   -- c <- fuN ( \ xs -> 2 <= sum ( map fromEnum xs )) [x,y,z]
@@ -208,11 +240,14 @@ fullAddM_explicit x y z = do
   assert_implies [not x, not z] [not c]
   return (r,c)
 
+assert_implies :: forall (m :: * -> *) s. (HasSAT s, MonadState s m) => [Bit] -> [Bit] -> m ()
 assert_implies xs ys =
   assertClause $ map not xs ++ ys
 
+xorN :: forall (m :: * -> *) s. (HasSAT s, MonadState s m) => [Bit] -> m Bit
 xorN = fuN $ foldr (/=) False
 
+fuN :: forall (m :: * -> *) s. (HasSAT s, MonadState s m) => ([Bool] -> Bool) -> [Bit] -> m Bit
 fuN f xs = do
   out <- exists
   let val = P.bool not id
