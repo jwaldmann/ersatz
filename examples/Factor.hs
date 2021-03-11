336ff3a4a7d9005e302ee6168cfaec7bab4f6914
Johannes Waldmann <waldmann@denk>
2021-03-11 12:52:58 +0100

    introduce cmd line argument

--- examples/Factor.hs
+++ examples/Factor.hs
@@ -1,22 +1,34 @@
+{-# language LambdaCase #-}
+
 module Main where
 
 import Ersatz
 import Control.Monad
+import System.Environment
 
-problem :: MonadSAT s m => m (Bits, Bits, Bits)
-problem = do
-  a <- liftM Bits (replicateM 10 exists)
-  b <- liftM Bits (replicateM 10 exists)
+problem :: MonadSAT s m => Integer -> m (Bits, Bits)
+problem n = do
+  let w = ceiling $ logBase 2 $ fromIntegral n
+  a <- liftM Bits (replicateM w exists)
+  b <- liftM Bits (replicateM w exists)
   let c = a * b
-  assert (a /== encode   1)
-  assert (b /== encode   1)
-  assert (c === encode 1001)
-  return (a,b,c)
+  assert (a /== encode 1)
+  assert (b /== encode 1)
+  assert (c === encode n)
+  return (a,b)
 
 main :: IO ()
-main = do
+main = getArgs >>= \ case
+ [] -> -- mainFor 1000000000001
+   mainFor 11111111111
+ [s] -> mainFor $ read s
+
+mainFor :: Integer -> IO ()
+mainFor n = do
   putStrLn "Solution:"
-  (Satisfied, Just (a,b,c)) <-
-    -- solveWith cryptominisat5 problem
-    solveWith minisat problem
-  putStrLn (show a ++ " * " ++ show b ++ " = " ++ show c)
+  (Satisfied, Just (a,b)) <-
+    -- solveWith cryptominisat5 $ problem n
+    solveWith minisat $ problem n
+  putStrLn $ unwords
+    [ "solver claims that", show n, "=", show a, "*", show b ]
+  when (a * b /= n) $ error $ unwords [ "but a * b =", show (a*b) ]
