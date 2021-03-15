5acf3d4e512b7c19240f48c3497a3b49a3288f8d
Johannes Waldmann <waldmann@denk>
2021-03-15 17:06:06 +0100

    Dadda-like multiplication (with overflow detection)

--- examples/Factor.hs
+++ examples/Factor.hs
@@ -1,16 +1,28 @@
-{-# language LambdaCase #-}
+{-# language LambdaCase, TypeApplications, PatternSignatures, KindSignatures, DataKinds, AllowAmbiguousTypes #-}
 
 module Main where
 
 import Ersatz
+import Ersatz.Number.Binary
 import Control.Monad
 import System.Environment
+import Data.Reflection
+import Data.Proxy
+import Data.Kind
+import GHC.TypeLits
 
-problem :: MonadSAT s m => Integer -> m (Bits, Bits)
+problem
+  :: forall (w ::Nat) s m
+  . (KnownNat w, MonadSAT s m)
+  => Integer
+  -> m (Binary w, Binary w)
+  --  -> m (Bits, Bits)
 problem n = do
-  let w = ceiling $ logBase 2 $ fromIntegral n
-  a <- liftM Bits (replicateM w exists)
-  b <- liftM Bits (replicateM w exists)
+  a :: Binary w <- exists
+  b <- exists
+  -- let w = fromIntegral $ natVal @w undefined
+  -- a <- Bits <$> replicateM w exists
+  -- b <- Bits <$> replicateM w exists
   let c = a * b
   assert (a /== encode 1)
   assert (b /== encode 1)
@@ -19,16 +31,24 @@ problem n = do
 
 main :: IO ()
 main = getArgs >>= \ case
- [] -> -- mainFor 1000000000001
+ [] -> do
+   mainFor 1000000000001
+   -- Bits: variables: 9457 clauses: 33485
+   -- Binary: variables: 6455 clauses: 25156
    mainFor 11111111111
+   -- Bits: variables: 6811 clauses: 24163
+   -- Binary: variables: 4629 clauses: 18030
  [s] -> mainFor $ read s
 
 mainFor :: Integer -> IO ()
 mainFor n = do
   putStrLn "Solution:"
-  (Satisfied, Just (a,b)) <-
-    -- solveWith cryptominisat5 $ problem n
-    solveWith minisat $ problem n
-  putStrLn $ unwords
-    [ "solver claims that", show n, "=", show a, "*", show b ]
-  when (a * b /= n) $ error $ unwords [ "but a * b =", show (a*b) ]
+  let w = ceiling $ logBase 2 $ fromIntegral n
+  reifyNat w $ \ (_ :: Proxy w) -> do
+      (Satisfied, Just (a,b)) <-
+        -- solveWith cryptominisat5 $ problem n
+        solveWith minisat $ problem @w n
+      putStrLn $ unwords
+        [ "solver claims that", show n, "=", show a, "*", show b ]
+      when (a * b /= n)
+        $ error $ unwords [ "but a * b =", show (a*b) ]
--- src/Ersatz/Number/Binary.hs
+++ src/Ersatz/Number/Binary.hs
@@ -2,11 +2,12 @@
 
 module Ersatz.Number.Binary where
 
-import Prelude hiding (and,or,not,(||),(&&))
+import Prelude hiding (any,all,and,or,not,(||),(&&))
 import Ersatz.Bit
-import Ersatz.Bits (Bits(..))
+import Ersatz.Bits (Bits(..),sumBits)
 import Ersatz.Number.Class
 import Ersatz.Codec
+import Ersatz.Variable
 import Ersatz.Equatable
 import Ersatz.Orderable
 
@@ -15,6 +16,10 @@ import Data.Proxy
 import Data.Reflection
 import Data.List (genericLength)
 import Control.Monad (replicateM)
+import qualified Data.Map as M
+import qualified Data.Sequence as Q
+
+import Debug.Trace
 
 -- | Numbers with bounded bit width and overflow detection.
 -- Semantics for arithmetical operations
@@ -35,6 +40,11 @@ instance KnownNat w => Codec (Binary w) where
     False -> decode s (Bits $ contents b)
     True -> error "overflow"
   encode n = fromBits $ encode n
+
+instance KnownNat w => Variable (Binary w) where
+  literally lit =
+    let w = fromIntegral $ natVal @w undefined
+    in  Binary <$> (replicateM w exists) <*> pure false
 
 -- | @x /== y@ is not the negation of  @x === y@.
 -- an overflowed number is never equal
@@ -85,4 +95,33 @@ instance forall w . KnownNat w => Num (Binary w) where
           in  c { overflow =
                   overflow a || overflow b || overflow c
                 }
+  a * b = 
+    let w = fromIntegral $ natVal @w undefined
+        c = multiply w (contents a) (contents b)
+        iszero x = all not $ contents x
+    in  Binary { contents = take w c
+               , overflow = -- not (iszero a && iszero b)
+                       ( overflow a || overflow b || c !! w )
+               }
 
+-- | compute binary representation of product,
+-- where bits at positions 0 .. w-1 are exact,
+-- and position w indicates overflow
+multiply w xs ys = reduce w $ M.fromListWith (<>) $ do
+  (i,x) <- zip [0..] xs
+  (j,y) <- zip [0..] ys
+  return (min w $ i+j, Q.singleton (x&&y))
+
+reduce
+  :: (Ord n, Enum n, Num n)
+  => n -> M.Map n (Q.Seq Bit) -> [Bit]
+reduce w m = case M.minViewWithKey m of
+  Just ((k,s), m') ->
+    if k < w
+    then
+      let Bits (b:bs) = sumBits s
+          cs = M.fromList
+            $ zip [k+1 .. w]
+            $ map Q.singleton bs
+      in  b : reduce w (M.unionWith (<>) m' cs)
+    else [ or $ map or $ M.elems m ]
